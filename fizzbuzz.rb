969ec57aebaf7836fa063dfb95093b1bb756da19
Sara Walker <sarawall1187@gmail.com>
2018-11-17 21:11:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(int)
- puts "#{"Fizz"} when the number is divisible by 3"
+ puts ""Fizz" when the number is divisible by 3"
  puts "#{"Buzz"} when the number is divisible by 5"
  puts "#{"FizzBuzz"} when the number is divisible by 3 and 5"
  puts "#{"nil"} when the number is not divisible by 3 or 5"
