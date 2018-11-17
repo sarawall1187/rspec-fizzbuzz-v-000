7c396d3562746f615a8e944ebdcccfdde9e0c66a
Sara Walker <sarawall1187@gmail.com>
2018-11-17 21:10:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
+def fizzbuzz(int)
+ puts "#{"Fizz"} when the number is divisible by 3"
+ puts "#{"Buzz"} when the number is divisible by 5"
+ puts "#{"FizzBuzz"} when the number is divisible by 3 and 5"
+ puts "#{"nil"} when the number is not divisible by 3 or 5"
+end
+
+def fizz
+  
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
